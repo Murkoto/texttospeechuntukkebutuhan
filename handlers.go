181d8808f79bfa4aa8056abdb2d912565723fda2
cesa.satria <cesa.satria@ui.ac.id>
2018-04-29 21:58:20 +0700

    buildpack

--- handlers.go
+++ handlers.go
@@ -6,7 +6,7 @@ func main() {
 	bot, err := linebot.New("6500e2b36c742d5f3988981a5cf30514", 
 		"90JkB4mN6PuwC506j/A+jSPlXWYGoW8ag6c5R9+R5AQRN0QiL46V0uZ+uZD+KBPo91gWoPWBuLV5QfiAPEM6FJkzeWDWO5o1SYZ9Pw4O1UYqF46e/CekYFLuBMg2oQSEYVUc37lIS/SEo1gcWtyZ/AdB04t89/1O/w1cDnyilFU=")
 
-	event, err := bot.ParseRequest(req)
+	event, error := bot.ParseRequest(req)
 	if err != nil {
 		
 	}
